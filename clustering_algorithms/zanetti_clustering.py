2be3b14b0aadee674c292320e6de1a8100633f94
GeorgeRLTyler <georgetyler123@hotmail.com>
2023-01-22 22:12:29 +0000

    current clustering algorithm to benchmark against

---
+++ clustering_algorithms/zanetti_clustering.py
@@ -0,0 +1,39 @@
+import numpy as np
+from sklearn.cluster import KMeans
+
+
+def DSBM_Clustering_Zanetti(adjacency_matrix,K,l,method='adjacency'):
+# input: np.array, adjacency_matrix --- A[i,j] = 1 if i -> j else 0.
+#        int, K --- number of blocks/communities.
+#        int, l --- number of eigenvectors to consider.
+# output: clusters
+
+    # construct Hermitian adjacency matrix
+    A_hat = adjacency_matrix
+    #A = (A + A.T)
+    A = (A_hat - A_hat.T)*1j
+
+    if method == 'laplacian':
+        # construct Laplacian matrix
+        D = np.diag(np.sum(np.abs(A),axis=0))
+        A = D - A
+        # calculating eigenvectors 
+        eig_vals, eig_vecs = np.linalg.eig(A)
+        #sorting according to largest in magnitude
+        idx = np.abs(eig_vals).argsort()
+    else:
+        eig_vals, eig_vecs = np.linalg.eig(A)
+        #sorting according to largest in magnitude
+        idx = np.abs(eig_vals).argsort()[::-1]
+    
+    eig_vecs = eig_vecs[:,idx][:,0:l]
+    eig_vecs_projected = np.zeros((len(A),len(A)))
+    for k in range(l):
+        eig_vecs_projected = eig_vecs_projected + np.outer(eig_vecs[:,k],eig_vecs[:,k].conj()) 
+    
+    eig_vecs_projected  = np.real(eig_vecs_projected)
+
+    kmeans = KMeans(n_clusters = K).fit(eig_vecs_projected)
+    clusters = kmeans.labels_
+
+    return clusters, eig_vecs_projected
